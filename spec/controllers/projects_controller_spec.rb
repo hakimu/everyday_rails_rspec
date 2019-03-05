466cfc703875ec3715584555898c862b75b329de
Hakimu Jackson <hakimu@newrelic.com>
2019-03-04 20:29:59 -0800

    Update factory and add controller spec

---
+++ spec/controllers/projects_controller_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+RSpec.describe ProjectsController, type: :controller do
+  describe '#index' do
+    context 'as an authenticated user' do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+      it 'responds successfully' do
+        sign_in @user
+        get :index
+        expect(response).to be_success
+      end
+
+      it 'returns a 200 response' do
+       sign_in @user
+       get :index
+       expect(response).to have_http_status '200'
+      end
+    end
+
+    context 'as a guest' do
+      it 'returns a 302 response' do
+        get :index
+        expect(response).to have_http_status '302'
+      end
+
+      it 'redirects to the sign-in page' do
+        get :index
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+  end
+  describe '#show' do
+    context 'as an authorized user' do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      it 'responds successfully' do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to be_success
+      end
+    end
+    
+    context 'as an unauthorized user' do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
+
+      it 'redirects to the dashboard' do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+  describe '#create' do
+    context 'as an authenticated user' do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      it 'adds a project' do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in @user
+        expect {
+          post :create, params: { project: project_params }
+        }.to change(@user.projects, :count).by(1)
+      end
+    end
+  end
+end
